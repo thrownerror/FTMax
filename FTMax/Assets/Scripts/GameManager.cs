9d77ffbc90ee9b2bc4e6f2e2b4087b7e8b5f5e58
Robert Bailey <emailrobertb@gmail.com>
2017-10-20 19:35:19 -0400

    Start of overworld code stub
    overwrold code stubbing

---
+++ FTMax/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private string gameMode;
+    public GameObject camera;
+    public GameObject overPlayer;
+    public GameObject battlePlayer;
+
+
+    private int playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        gameMode = "overworld";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void toggleMode(){
+        if(gameMode == "overworld"){
+            gameMode = "battle";
+        }
+        else{
+            gameMode = "overworld";
+        }
+    }
+
+    void setPlayerHealth()
+    {
+        //overPlayer.health = battlePlayer.health;
+    }
+
+}
