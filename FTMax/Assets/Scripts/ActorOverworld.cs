93a5e6f18aba718528313b282ea632a089f1fc7b
Robert Bailey <emailrobertb@gmail.com>
2017-10-20 20:43:08 -0400

    Stubbing in actorOverworld class

---
+++ FTMax/Assets/Scripts/ActorOverworld.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorOverworld : MonoBehaviour {
+
+    float collisionRadius;
+    GameObject currentTarget;
+    string team; //enemy, obstacle, player, friendly
+    float speed;
+    Vector3 lineToTarget;
+    float distance;
+
+    public ActorOverworld(string side)
+    {
+        team = side;
+    }
+    public ActorOverworld()
+    {
+        team = "obstacle";
+    }
+
+    void move()
+    {
+
+    }
+
+    void getLineToTarget()
+    {
+        lineToTarget = currentTarget.transform.position - this.transform.position;
+    }
+    void distanceCalculator(Vector3 target)
+    {
+
+    }
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
