2ba58b9a9a0a81a97709d5fea48e47f7443b9877
Robert Bailey <emailrobertb@gmail.com>
2017-10-21 20:27:45 -0400

    Randomized AI movement
    Setup placed in Overworld

--- FTMax/Assets/Scripts/OverworldScripts/enemyOverworld.cs
+++ FTMax/Assets/Scripts/OverworldScripts/enemyOverworld.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class enemyOverworld : AgentOverworld
 {
+    float angle;
+    public Rigidbody rb;
 
     public enemyOverworld() //:// base("player", .01f, .03f, .5f)
     {
@@ -13,6 +15,7 @@ public class enemyOverworld : AgentOverworld
     // Use this for initialization
     void Start()
     {
+        angle = 0;
         // base("player", .01f, .03f, .5f);
         base.setProperties("enemy", 0.1f, .02f, .5f);
     }
@@ -26,6 +29,47 @@ public class enemyOverworld : AgentOverworld
     // Update is called once per frame
     void Update()
     {
+        move();
+    }
 
+    void move()
+    {
+        Vector3 moveDir = new Vector3(0, 0, 0);
+        
+        int random = (int)Random.Range(0f, 5.999f);
+        Debug.Log(random);
+        switch (random)
+        {
+            case 0:
+                moveDir = moveDir + (transform.forward * Time.deltaTime);
+                break;
+            case 1:
+                moveDir = moveDir + (transform.forward * Time.deltaTime * -1);
+                break;
+            case 2:
+                moveDir = moveDir + (transform.forward * Time.deltaTime * -1);
+                angle -= 40 * Time.deltaTime;
+                break;
+            case 3:
+                moveDir = moveDir + (transform.forward * Time.deltaTime * -1);
+                angle += 40 * Time.deltaTime;
+                break;
+            case 4:
+                moveDir = moveDir + (transform.forward * Time.deltaTime);
+                angle += 40 * Time.deltaTime;
+                break;
+            case 5:
+                moveDir = moveDir + (transform.forward * Time.deltaTime);
+                angle -= 40 * Time.deltaTime;
+                break;
+            default:
+                break;
+        }
+        rb.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        moveDir.Normalize();
+        moveDir = moveDir * (acceleration * Time.deltaTime);
+        rb.velocity += moveDir;
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, base.maxSpeed);
+        rb.position += rb.velocity;
     }
 }
